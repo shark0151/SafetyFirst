3c58fd3ebc517583579225360eb13a08c1d48661
Alex F <vlad.faget@outlook.com>
2021-09-13 14:14:27 +0200

    stuff

---
+++ Source/SafetyF/SafetyFGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SafetyFGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAFETYF_API ASafetyFGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
