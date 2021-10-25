d6f38ccbd65e39906f677836495dcaebfa42c7f0
Alex F <vlad.faget@outlook.com>
2021-10-25 11:31:59 +0200

    b

---
+++ Source/SafetyF/MyWheeledVehicle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehicleMovementComponent4W.h"
+#include "MyWheeledVehicle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAFETYF_API UMyWheeledVehicle : public UWheeledVehicleMovementComponent4W
+{
+	GENERATED_BODY()
+	
+};
